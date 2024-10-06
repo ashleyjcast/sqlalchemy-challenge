938fc200bfa23a46904a22d15fd7c3dc1a7cf57c
ashleyjcast <ashleyj.castaneda@gmail.com>
2024-10-06 07:36:01 -0400

    complete

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,5 +1,7 @@
 # Import the dependencies.
 import numpy as np
+import datetime as dt
+import pandas as pd
 import sqlalchemy
 from flask import Flask, jsonify
 from sqlalchemy import create_engine, func
@@ -51,8 +53,9 @@ def welcome():
 # Precipitation route
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    most_recent = session.query(measurement.date).order_by(measurement.date.desc()).first()
-    previous_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+    most_recent = session.query(measurement.date).order_by(measurement.date.desc()).first()[0]
+    most_recent = dt.datetime.strptime(most_recent, '%Y-%m-%d')
+    previous_year = most_recent - dt.timedelta(days=365)
     date_prcp = session.query(measurement.date,measurement.prcp).filter(measurement.date >= previous_year).all()
     prcp_data = {date:prcp for date, prcp in date_prcp}
     return jsonify(prcp_data)
@@ -60,17 +63,52 @@ def precipitation():
 # Stations route
 @app.route("/api/v1.0/stations")
 def stations():
-    
+    all_stations = session.query(station).all()
+    station_list = [station.name for station in all_stations]
+    return jsonify(station_list)
 
 
 # Tobs route
 @app.route("/api/v1.0/tobs")
 def tobs():
+    most_active_stations = session.query(measurement.station, func.count(measurement.station)).\
+        group_by(measurement.station).\
+        order_by(func.count(measurement.station).desc()).all()
+    stations_id = [station[0] for station in most_active_stations]
+    most_recent = session.query(measurement.date).order_by(measurement.date.desc()).first()[0]
+    most_recent = dt.datetime.strptime(most_recent, '%Y-%m-%d')
+    previous_year = most_recent - dt.timedelta(days=365)
+    recent_year_observations = session.query(measurement.date, measurement.tobs).\
+        filter(measurement.station.in_(stations_id)).\
+        filter(measurement.date >= previous_year).all()
+    tobs_data = [{"date": date, "tobs": tobs} for date, tobs in recent_year_observations]
+    return jsonify(tobs_data)
+
 
 # Temp Routes
 @app.route("/api/v1.0/<start>")
 @app.route("/api/v1.0/<start>/<end>")
 def temp_stats(start, end=None):
+    if not end:
+        station_temps = session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
+                        filter(measurement.station >= start).all()
+    else:
+        station_temps = session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
+                        filter(measurement.station >= start).\
+                        filter(measurement.date <= end).all()
+    if station_temps:
+        temp_results = {
+            "TMIN": station_temps[0][0], 
+            "TAVG": station_temps[0][1], 
+            "TMAX": station_temps[0][2]
+        }
+    else:
+        temp_results = {
+            "TMIN": None, 
+            "TAVG": None, 
+            "TMAX": None
+            }
+    return jsonify(temp_results)
 
 if __name__ == '__main__':
     app.run(debug=True)
